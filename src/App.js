aa0cd5c8d407f468f83203f90ef8ad3e4222548b
Tony Lau <99046505+intrepidcanadian@users.noreply.github.com>
2023-10-10 20:49:13 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Name from './components/Name';
 
 function App() {
+
+
+const [name, setName ] = useState("");
+const [fruit, setFruit] = useState("");
+const [count, setCount] = useState(0);
+const [render, setRender] = useState(false);
+
+const handleAppleValue = (value) => {
+  console.log("Received value from Name component:", value);
+  setFruit(value);
+}
+
+function conditional() {
+
+  setRender((prevState) => !prevState);
+
+}
+
+
+useEffect(() => {
+  setName("hello");
+
+  const test = setInterval(() =>{
+
+    console.log("Hi");
+    setCount(prevCount => prevCount + 1);
+
+    console.log(count);
+    
+    }, 1000);  
+
+    return () => clearInterval(test);
+    
+}, [count]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <Name nameValue = {name} onAppleValue={handleAppleValue} />
+      <p>{fruit}</p>
+      {render && <p>{count}</p> }
+      <button onClick = {() => conditional()}>Show Count </button>
+
       </header>
     </div>
   );
